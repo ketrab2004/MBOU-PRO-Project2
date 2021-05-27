0788c94b2bdedbbdaa529611a6a52b727b6268b4
ketrab2004 <bartek@fos.nl>
2021-05-27 13:56:56 +0200

    Added static function to check if given Any is of type Item or something descending from Item usage: Item.checkIfItem(a: Any)

--- src/main/kotlin/classes/item/Item.kt
+++ src/main/kotlin/classes/item/Item.kt
@@ -21,4 +21,31 @@ open class Item(val name: String,
     open var usableCommands: List<ItemCommands> = listOf(); //list of usable commands
 
     var amount: Int = 1;
+
+    //static function
+    companion object {
+        /**
+         * Checks if given Any is a descendant class of item (or an item)
+         * @return true if it is a item, false if not
+         */
+        fun checkIfItem(input: Any) : Boolean{
+            when (input){
+                is Item -> {
+                    return true
+                }
+                is Weapon -> {
+                    return true
+                }
+                is Armor -> {
+                    return true
+                }
+                is Consumable -> {
+                    return true
+                }
+                else -> {
+                    return false
+                }
+            }
+        }
+    }
 }

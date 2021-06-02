baab64cc1b83f60a932a4a82199dba5d305eeeba
ketrab2004 <bartek@fos.nl>
2021-06-02 11:17:20 +0200

    Moved doCommand into Item class in a region

--- src/main/kotlin/classes/item/Item.kt
+++ src/main/kotlin/classes/item/Item.kt
@@ -47,5 +47,114 @@ open class Item(val name: String,
                 }
             }
         }
+
+        /**
+         * Checks if given Any is a descendant class of item (or an item) and returns it's name
+         * @return the name of the item, NULL if not an item
+         */
+        fun getName(item: Any) : String{
+            when (item){
+                is Item -> {
+                    return item.name
+                }
+                is Weapon -> {
+                    return item.name
+                }
+                is Armor -> {
+                    return item.name
+                }
+                is Consumable -> {
+                    return item.name
+                }
+                else -> {
+                    println("⚠ $item is not an item. ⚠")
+                    return "null"
+                }
+            }
+        }
     }
+
+    /* region item Do Command */
+    /**
+     * Does a command based on the input
+     */
+    public fun doCommandItem(input: String){
+        val arguments: List<String> = input.split(" ");
+
+
+
+        when(arguments[0].toLowerCase()){ //switch case
+            "help" -> {
+                commandHelp(arguments)
+            }
+            //Go back
+            "back" -> {
+                commandBack(arguments)
+            }
+
+            //Inspect aliases
+            "inspect" -> {
+                commandInspect(arguments)
+            }
+            "info" -> {
+                commandInspect(arguments)
+            }
+
+            //Consume aliases
+            "consume" -> {
+                commandConsume(arguments)
+            }
+            "eat" -> {
+                commandConsume(arguments,)
+            }
+            "drink" -> {
+                commandConsume(arguments)
+            }
+
+            //Equip aliases
+            "equip" -> {
+                commandEquip(arguments)
+            }
+            "wear" -> {
+                commandEquip(arguments)
+            }
+
+
+            else -> {
+                println("⚠ '${arguments[0]}' is not a known command.")
+            }
+        }
+    }
+
+    private fun commandHelp(args: List<String>){
+        val padding: Int = 35; //using padEnd to add spaces to set a length, that way all the text lines up nicely
+        println("Commands:")
+        println("* help".padEnd(padding)                   +"Shows all commands you can do.")
+        println("* inspect, info".padEnd(padding)          +"Inspect the item to see it's description.")
+        println("* consume, eat, drink".padEnd(padding)    +"Consume the item.")
+        println("* equip, wear".padEnd(padding)            +"Equip the item in the first open slot.")
+        println("* back".padEnd(padding)                   +"Return to the previous menu.")
+    }
+    private fun commandInspect(args: List<String>){
+        println("You inspect $name.")
+        println(description)
+    }
+    private fun commandConsume(args: List<String>){
+        if(usableCommands.contains(ItemCommands.CONSUME)) { //consume is a usable command
+
+        }else {
+            println("You cannot consume '$name'.")
+        }
+    }
+    private fun commandEquip(args: List<String>){
+        if(usableCommands.contains(ItemCommands.EQUIP)) { //equip is a usable command
+
+        }else {
+            println("You cannot equip '$name'.")
+        }
+    }
+    private fun commandBack(args: List<String>){
+        //TODO go back
+    }
+    /* endregion */
 }

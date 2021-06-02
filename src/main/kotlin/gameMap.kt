de0b609545e19a55da59b6695b80cc70ba1366b8
ketrab2004 <bartek@fos.nl>
2021-06-02 13:14:08 +0200

    Created gameMap file

---
+++ src/main/kotlin/gameMap.kt
@@ -0,0 +1,23 @@
+import classes.Room
+
+/**
+ * array with lists of rooms
+ *
+ * each list in the array is an level/floor
+ *
+ * each room in each list is a room
+ *
+ * 8 levels/floors
+ */
+public fun getGameMap(): Array<List<Room>> {
+    var gameMap: Array<List<Room>> =
+        arrayOf(listOf(), listOf(), listOf(), listOf(), listOf(), listOf(), listOf(), listOf())
+
+    var room = Room("Main hallway", "A big hallway leading through the entire bottom floor of the castle", listOf())
+    //TODO add POIs
+    var level0: List<Room> = listOf(room)
+    gameMap[0] = level0
+
+
+    return gameMap;
+}

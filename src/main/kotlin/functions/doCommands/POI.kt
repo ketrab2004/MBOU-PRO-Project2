1eda49eca4f404303f84b089a0d2ed4a444bbb3f
ketrab2004 <bartek@fos.nl>
2021-06-09 13:46:56 +0200

    Big work in doCommand POI * Made help display the correct commands * added aliases to commands * added some commands to the switch case * made commands for (not fully but there are TODOs)

--- src/main/kotlin/functions/doCommands/POI.kt
+++ src/main/kotlin/functions/doCommands/POI.kt
@@ -1,10 +1,11 @@
 package functions.doCommands
 
-import classes.POI
-import classes.PossiblePOICommands
-import classes.POIType
+import classes.Player
+import classes.Room
+import classes.item.Item
+import classes.poi.*
 
-public fun doCommandPOI(input: String, POI: POI){
+public fun doCommandPOI(input: String, POI: POI, plr: Player){
     val arguments: List<String> = input.split(" ");
 
     when(arguments[0].toLowerCase()){ //switch case
@@ -16,9 +17,17 @@ public fun doCommandPOI(input: String, POI: POI){
             commandBack(arguments)
         }
 
+        //Inspect aliases
+        "inspect" ->{
+            commandInspect(arguments, POI)
+        }
+        "info" ->{
+            commandInspect(arguments, POI)
+        }
+
         //Enter aliases
         "enter" -> {
-            commandEnter(arguments, POI)
+            commandEnter(arguments, POI, plr)
         }
 
 
@@ -32,10 +41,18 @@ public fun doCommandPOI(input: String, POI: POI){
 
         //Pickup aliases
         "pickup" -> {
-            commandPickup(arguments, POI)
+            commandPickup(arguments, POI, plr)
         }
-        "pickup" -> {
-            commandPickup(arguments, POI)
+        "grab" -> {
+            commandPickup(arguments, POI, plr)
+        }
+
+        //Destroy aliases
+        "destroy" ->{
+            commandDestroy(arguments, POI, plr)
+        }
+        "break" ->{
+            commandDestroy(arguments, POI, plr)
         }
 
 
@@ -49,58 +66,82 @@ private fun commandHelp(args: List<String>){
     // TODO only show relevant commands
     val padding: Int = 35; //using padEnd to add spaces to set a length, that way all the text lines up nicely
     println("Commands:")
-    println("* help".padEnd(padding)                   +"Shows all commands you can do.")
-    println("* inspect, info".padEnd(padding)          +"Inspect the item to see it's description.")
-    println("* consume, eat, drink".padEnd(padding)    +"Consume the item.")
-    println("* equip, wear".padEnd(padding)            +"Equip the item in the first open slot.")
-    println("* back".padEnd(padding)                   +"Return to the previous menu.")
+    println("* help".padEnd(padding)                    +"Shows all commands you can do.")
+    println("* inspect, info".padEnd(padding)           +"Inspect the object to see it's description.")
+    println("* enter".padEnd(padding)                   +"Enter the door/staircase.")
+    println("* open, search".padEnd(padding)            +"Open the object to see it's content.")
+    println("* destroy, break".padEnd(padding)            +"Destroy the object.")
+    println("* back".padEnd(padding)                    +"Return to the previous menu.")
 }
 private fun commandInspect(args: List<String> , POIClass: POI){
     println("You inspect ${POIClass.name}.")
     println(POIClass.description)
 }
-private fun commandEnter(args: List<String>, POIClass: POI){
-    if(POIClass.usableCommands.contains(PossiblePOICommands.ENTER)) { //consume is a usable command
-        if (POIClass.isLocked)
-        {
-            //TODO Check player inventory for the right item to unlock POI
-            println("You do not have the right item to unlock this '${POIClass.name}'")
+private fun commandEnter(args: List<String>, POI: POI, plr: Player){
+    if(POI.usableCommands.contains(PossiblePOICommands.ENTER)) { //consume is a usable command
+        val isLocked = POI.properties["IsLocked"]
+        if (isLocked is Boolean){
+            if (isLocked){
+                println("This '${POI.name}' is locked and needs to be unlocked before it can be entered.");
+                //TODO loop through items to find the key
+
+            }else{ //door is not locked
+                //TODO use staircase/door
+            }
+        }else{ //door is not locked
+            //TODO use staircase/door
         }
-        else
-        {
-            println("You open the '${POIClass.name}' and enter it.")
-            //TODO Send player to new room
+    }else {
+        println("You cannot enter '${POI.name}'.")
+    }
+}
+private fun commandOpen(args: List<String>, POI: POI){
+    if(POI.usableCommands.contains(PossiblePOICommands.OPEN)) { //open is a usable command
+        val isLocked = POI.properties["IsLocked"]
+        if (isLocked is Boolean){
+            if (isLocked){
+                println("This '${POI.name}' is locked and needs to be unlocked before it can be opened.");
+                //TODO loop through items to find the key
+
+            }else{ //container is not locked
+                //TODO open container
+            }
+        }else{ //container does not have a isLocked var
+            //TODO open container
         }
     }else {
-        println("You cannot enter '${POIClass.name}'.")
+        println("You cannot open '${POI.name}'.")
     }
 }
-private fun commandOpen(args: List<String>, POIClass: POI){
-    if(POIClass.usableCommands.contains(PossiblePOICommands.OPEN)) { //open is a usable command
-        // TODO open inventory of Classes.POI
+private fun commandPickup(args: List<String>, POI: POI, plr: Player){
+    if(POI.usableCommands.contains(PossiblePOICommands.PICKUP)) { //pickup is a usable command
+        val pickup = POI.properties["Pickup"];
+        if (pickup is Item){
+            plr.inventory.add(pickup)
+
+            println("You acquired '${pickup.name}'.")
+
+            GlobalGameMap.gameMap[plr.currentLevel][plr.currentRoom].poiList.remove(POI)
+        }else{ //poi has pickup as possible command but doesn't contain an item to pickup
+            println("You cannot pick up '${POI.name}'.")
+        }
     }else {
-        println("You cannot open '${POIClass.name}'.")
+        println("You cannot pick up '${POI.name}'.")
     }
 }
-private fun commandPickup(args: List<String>, POIClass: POI){
-    if(POIClass.usableCommands.contains(PossiblePOICommands.PICKUP)) { //pickup is a usable command
-        // TODO add item to inventory
+private fun commandDestroy(args: List<String>, POI: POI, plr: Player){
+    if(POI.usableCommands.contains(PossiblePOICommands.DESTROY)) { //equip is a usable command
+        println("You destroyed '${POI.name}'.")
+        GlobalGameMap.gameMap[plr.currentLevel][plr.currentRoom].poiList.remove(POI)
     }else {
-        println("You cannot pick up '${POIClass.name}'.")
+        println("You cannot destroy '${POI.name}'.")
     }
 }
-private fun commandDestroy(args: List<String>, POIClass: POI){
-    if(POIClass.usableCommands.contains(PossiblePOICommands.DESTROY)) { //equip is a usable command
-        // TODO destroy the object from the room
-    }else {
-        println("You cannot destroy '${POIClass.name}'.")
-    }
-}
-private fun commandUse(args: List<String>, POIClass: POI){
-    if(POIClass.usableCommands.contains(PossiblePOICommands.USE)) { //equip is a usable command
+private fun commandUse(args: List<String>, POI: POI){
+    if(POI.usableCommands.contains(PossiblePOICommands.USE)) { //equip is a usable command
 
     }else {
-        println("There is no way to use '${POIClass.name}'.")
+        println("There is no way to use '${POI.name}'.")
     }
 }
 private fun commandBack(args: List<String>){

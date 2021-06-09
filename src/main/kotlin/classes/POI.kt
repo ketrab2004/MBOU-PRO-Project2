7b3e15c70c0ce4b54fe85f13fe330001784c9272
ketrab2004 <bartek@fos.nl>
2021-06-09 12:49:39 +0200

    Reworked POI Added POIType Created list of know properties

--- src/main/kotlin/classes/POI.kt
+++ src/main/kotlin/classes/POI.kt
@@ -1,16 +1,34 @@
-package classes.POI
+package classes
 
 enum class PossiblePOICommands {
     ENTER,
     OPEN,
-    PICKUP,
-    DESTROY,
-    USE,
+    PICKUP
+}
+enum class POIType{
+    MISC,
+    DOOR,
+    STAIRCASE,
+    CONTAINER,
+    ITEM
 }
 
 
-open class POI (val name: String,
-                val description: String,
-                val usableCommands : List<PossiblePOICommands> = listOf()){
+class POI (val name: String, val description: String){
 
+    var type: POIType = POIType.MISC;
+
+    var usableCommands: List<PossiblePOICommands> = listOf()
+
+    /**
+     * ## List of known properties:
+     * - "TargetRoom" [Int]; Index of room behind this door
+     * - "TargetFloor" [Int]; Index of floor/level up/down these stairs
+     * - "IsLocked" [Boolean]; Whether this door/staircase is locked
+     * - "Key" [String]; Code that a key needs to have to be able to unlock this (also works on containers)
+     * - "Content" [MutableList]<[Item]>; List of items inside of this POI
+     * - "Pickup" [Item]; Used when POI is just a single item on the ground
+     * ```
+     */
+    var properties: Map<String, Any> = mapOf(); //like NBT tags in Minecraft
 }

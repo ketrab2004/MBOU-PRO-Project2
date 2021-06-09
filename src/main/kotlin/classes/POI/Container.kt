5d96de280e5ea61560fc3ceb2446ff43442a0e2c
ketrab2004 <bartek@fos.nl>
2021-06-09 12:47:31 +0200

    Removed old POI classes

--- src/main/kotlin/classes/POI/Container.kt
+++
@@ -1,9 +0,0 @@
-package classes.POI
-
-class Container (name: String,
-                 description: String,
-                 usableCommands : List<PossiblePOICommands> = listOf())
-            : POI(name, description, usableCommands){ //Extends POI
-    var isLocked: Boolean = false;
-    var inventory: List<classes.Item> = listOf()
-}
--- src/main/kotlin/classes/POI/Item.kt
+++
@@ -1,4 +0,0 @@
-package classes.POI
-
-class Item {
-}

80dd6c822e303acbb5efdc0923d8b0f24439e121
ketrab2004 <bartek@fos.nl>
2021-06-09 13:44:38 +0200

    This should've been commited with the other global gamemap commit

--- src/main/kotlin/classes/GlobalGameMap.kt
+++ src/main/kotlin/classes/GlobalGameMap.kt
@@ -1,18 +1,27 @@
 import classes.Room
 
-/**
- * array with lists of rooms
- *
- * each list in the array is an level/floor
- *
- * each room in each list is a room
- *
- * 8 levels/floors
- */
-public var gameMap: Array<List<Room>> =
-    arrayOf(listOf(), listOf(), listOf(), listOf(), listOf(), listOf(), listOf(), listOf())
+class GlobalGameMap {
+    //static
+    companion object {
+        /**
+         * array with lists of rooms
+         *
+         * each list in the array is an level/floor
+         *
+         * each room in each list is a room
+         *
+         * 8 levels/floors
+         */
+        public var gameMap: Array<List<Room>> =
+            arrayOf(listOf(), listOf(), listOf(), listOf(), listOf(), listOf(), listOf(), listOf())
 
-var room = Room("Main hallway", "A big hallway leading through the entire bottom floor of the castle", listOf())
-//TODO add POIs
-var level0: List<Room> = listOf(room)
-gameMap[0] = level0
+        public fun setupGameMap() {
+            var room =
+                Room("Main hallway", "A big hallway leading through the entire bottom floor of the castle")
+
+            //TODO add POIs
+            var level0: List<Room> = listOf(room)
+            gameMap[0] = level0
+        }
+    }
+}

eaf8bbf53ed2c19db2d2f9b8da0516c0614538f9
ketrab2004 <bartek@fos.nl>
2021-06-09 11:33:58 +0200

    Added addHealth function Made health var Made inventory mutable (so it can be edited) Made equipped an array of item?s (? because each slot can be empty) (alsot set to size of amount of armor slots)

--- src/main/kotlin/classes/Enemy.kt
+++ src/main/kotlin/classes/Enemy.kt
@@ -1,16 +1,29 @@
 package classes
 
 import classes.Item
+import classes.ItemArmorSlot;
+import kotlin.math.min
 
 open class Enemy(
     val name: String,
     val description: String,
     val maxHealth: Float) {
 
-    public val health: Float = maxHealth;
+    public var health: Float = maxHealth;
 
-    public var inventory: List<Item> = listOf();
+    public var inventory: MutableList<Item> = mutableListOf(); //mutable list can be edited
 
-    public var equipped: List<Item> = listOf();
+    public var equipped: Array<Item?> = arrayOfNulls<Item?>(ItemArmorSlot.values().size); //size of amount of armor slots
 
+    /**
+     * Add health
+     * @return amount of health gained
+     */
+    public fun addHealth(eatPoints: Float): Float{
+        val toReturn = min(health + eatPoints, maxHealth) - health;
+
+        health += toReturn;
+
+        return toReturn;
+    }
 }

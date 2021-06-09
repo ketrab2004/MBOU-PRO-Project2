1b35e8676110499b9e1b9cb4cc2fe2c5d59bc2ab
ketrab2004 <bartek@fos.nl>
2021-06-09 13:44:51 +0200

    Added poi import to room

--- src/main/kotlin/classes/Room.kt
+++ src/main/kotlin/classes/Room.kt
@@ -1,5 +1,7 @@
 package classes
 
+import classes.poi.POI
+
 class Room (val name: String, val description: String){
 
     var poiList: MutableList<POI> = mutableListOf() //mutable so it can be edited

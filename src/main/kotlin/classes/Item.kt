f403d3f0b2712d406e541b7e5510f14e9da2b270
ketrab2004 <bartek@fos.nl>
2021-06-09 12:48:56 +0200

    Changed formatting of list of know properties

--- src/main/kotlin/classes/Item.kt
+++ src/main/kotlin/classes/Item.kt
@@ -20,21 +20,17 @@ enum class ItemArmorSlot(val index: Int){
     OFFHAND(4)
 }
 
-class Item(val name: String,
-           val description: String,
-           val maxStackSize: Int) {
+class Item(val name: String, val description: String, val maxStackSize: Int) {
 
     var type: ItemType = ItemType.MISC;
 
     var usableCommands: List<ItemCommands> = listOf(); //list of usable commands
 
     /**
-     * List of known properties:
-     * 
-     * ``` (so formatting is kept)
-     * - "EatPoints" [Float]            How much health you gain when consuming it
-     * - "ArmorSlot" [ItemArmorSlot]    In what slot this item can be equipped
-     * ```
+     * ## List of known properties:
+     * - "EatPoints" [Float]; How much health you gain when consuming it
+     * - "ArmorSlot" [ItemArmorSlot]; In what slot this item can be equipped
+     * - "Key" [String]; Key what gets used to unlock a door
      */
     var properties: Map<String, Any> = mapOf(); //like NBT tags in Minecraft
 

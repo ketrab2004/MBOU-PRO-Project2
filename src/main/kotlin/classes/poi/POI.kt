6c1136bdeb40e7d4f1249399afc18314e6a4ca56
ketrab2004 <bartek@fos.nl>
2021-06-10 12:29:10 +0200

    Added static functions for creating special POIs

--- src/main/kotlin/classes/poi/POI.kt
+++ src/main/kotlin/classes/poi/POI.kt
@@ -1,5 +1,7 @@
 package classes.poi
 
+import classes.item.Item;
+
 open class POI (val name: String, val description: String){
 
     var type: POIType = POIType.MISC;
@@ -15,5 +17,72 @@ open class POI (val name: String, val description: String){
      * - "Content" [MutableList]<[Item]>; List of items inside of this POI
      * - "Pickup" [Item]; Used when POI is just a single item on the ground
      */
-    var properties: Map<String, Any> = mapOf(); //like NBT tags in Minecraft
+    var properties: MutableMap<String, Any> = mutableMapOf(); //like NBT tags in Minecraft
+
+    //static stuff goes in here
+    companion object {
+        /**
+         * Creates a door poi and returns it
+         * @param[targetRoom] index of room behind door
+         * @param[key] keycode required to unlock this door (null if not locked)
+         */
+        public fun createDoor(name: String, description: String, targetRoom: Int, key: String?): POI {
+            var poi = POI(name, description);
+
+            poi.properties["TargetRoom"] = targetRoom;
+            if (key != null){ //if key is given
+                poi.properties["Key"] = key;
+                poi.properties["IsLocked"] = true;
+            }
+
+            poi.type = POIType.DOOR;
+
+            return poi;
+        }
+
+        /**
+         * Creates a staircase poi and returns it
+         * @param[targetFloor] index of room behind door
+         * @param[key] keycode required to unlock this door (null if not locked)
+         */
+        public fun createStaircase(name: String, description: String, targetFloor: Int, key: String?): POI {
+            var poi = POI(name, description);
+
+            poi.properties["TargetFloor"] = targetFloor;
+            if (key != null){ //if key is given
+                poi.properties["Key"] = key;
+                poi.properties["IsLocked"] = true;
+            }
+
+            poi.type = POIType.STAIRCASE;
+
+            return poi;
+        }
+
+        /**
+         * Creates a container poi and returns it
+         * @param[content] mutableList (so items can be removed from it) of Items
+         */
+        public fun createContainer(name: String, description: String, content: MutableList<Item>): POI {
+            var poi = POI(name, description);
+
+            poi.properties["Content"] = content;
+            poi.type = POIType.CONTAINER;
+
+            return poi;
+        }
+
+        /**
+         * Creates a pickup poi and returns it
+         * @param[pickup] item that will be picked up
+         */
+        public fun createPickup(name: String, description: String, pickup: Item): POI {
+            var poi = POI(name, description);
+
+            poi.properties["Content"] = pickup;
+            poi.type = POIType.ITEM;
+
+            return poi;
+        }
+    }
 }

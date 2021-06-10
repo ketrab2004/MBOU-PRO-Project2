6d1d620f45edd6f08bc88a1f515c232a36cf2c8d
ketrab2004 <bartek@fos.nl>
2021-06-10 12:35:45 +0200

    Added comments to possiblePOICommands

--- src/main/kotlin/classes/poi/PossiblePOICommands.kt
+++ src/main/kotlin/classes/poi/PossiblePOICommands.kt
@@ -1,7 +1,14 @@
 package classes.poi
 
 enum class PossiblePOICommands {
+    /**
+     * for entering stairs/doors
+     */
     ENTER,
+
+    /**
+     * for opening containers
+     */
     OPEN,
     PICKUP,
     DESTROY,

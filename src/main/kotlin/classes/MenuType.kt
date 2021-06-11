4c5cca8f5918ea48b42937f5c72785fa53d8fd0c
ketrab2004 <bartek@fos.nl>
2021-06-11 15:37:21 +0200

    Added more menuTypes (inInventory and inRoom) Also added comments describing them

--- src/main/kotlin/classes/MenuType.kt
+++ src/main/kotlin/classes/MenuType.kt
@@ -1,8 +1,32 @@
 package classes
 
 enum class MenuType() {
+    /**
+     * Highest "level" of menuType
+     * (like in a folder structure)
+     */
     NONE,
+
+    /**
+     * Looking through your inventory
+     */
     INVENTORY,
+
+    /**
+     * After having chosen an item from your inventory
+     */
+    IN_INVENTORY,
+
+    /**
+     * Looking through the room
+     */
     ROOM,
+
+    /**
+     * After having chosen an poi from the current room
+     */
+    IN_ROOM,
+
+
     BATTLE
 }

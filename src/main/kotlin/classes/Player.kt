d0798dcafd4ce659208c764a265e07308164ed2d
ketrab2004 <bartek@fos.nl>
2021-06-11 18:08:55 +0200

    Added currentMenuIndex 1 for keeping track of which item inside of currentMenuIndex

--- src/main/kotlin/classes/Player.kt
+++ src/main/kotlin/classes/Player.kt
@@ -7,7 +7,7 @@ class Player(name: String, description: String, maxHealth: Float)
     var currentRoom: Int = 0; //index of room in gameMap array > list
 
     var currentMenu: MenuType = MenuType.NONE
-    var currentMenuIndex: Int = 0; //if in a inventory menu this index points to which one
-
+    var currentMenuIndex: Int = 0; //if in a inventory menu this index points to which item
+    var currentMenuIndex1: Int = 0; //if in a container this index points to which item inside of it
 
 }

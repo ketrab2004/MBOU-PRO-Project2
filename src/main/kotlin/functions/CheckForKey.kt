225cbd9e9ce9d59a38cc2445926628290c24fdc0
ketrab2004 <bartek@fos.nl>
2021-06-16 12:35:15 +0200

    Made checkForKey also return item for printing

--- src/main/kotlin/functions/CheckForKey.kt
+++ src/main/kotlin/functions/CheckForKey.kt
@@ -1,6 +1,7 @@
 package functions
 
 import classes.Player
+import classes.item.Item
 import classes.item.ItemType
 
 /**
@@ -10,17 +11,18 @@ import classes.item.ItemType
  * @param[key] keyCode to look for
  *
  * @return true if a key has been found, false if not
+ * also returns item key
  */
-public fun checkForKey(plr: Player, key: String): Boolean{
+public fun checkForKey(plr: Player, key: String): Pair<Boolean, Item?>{
     plr.inventory.forEach {
         if (it.type == ItemType.KEY){ //item is a key
             val itKey = it.properties["Key"]
             if (itKey is String){ //key property has been set
                 if (itKey == key){ //key matches
-                    return true; //found so return true
+                    return Pair(true, it); //found so return true with key
                 }
             }
         }
     }
-    return false; //return false because nothing was found
+    return Pair(false, null); //return false because nothing was found
 }

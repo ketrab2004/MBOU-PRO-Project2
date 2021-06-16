f396194fe4c2ec62a1b1ff17d4386079ddabf751
ketrab2004 <bartek@fos.nl>
2021-06-16 12:21:52 +0200

    Created function to take damage which returns damage done and whether the enemy survived (in the form a pair)

--- src/main/kotlin/classes/Enemy.kt
+++ src/main/kotlin/classes/Enemy.kt
@@ -2,6 +2,7 @@ package classes
 
 import classes.item.Item
 import classes.item.ItemArmorSlot;
+import classes.item.ItemType
 import kotlin.math.min
 
 open class Enemy(
@@ -43,5 +44,32 @@ open class Enemy(
         }
 
         return 1-damage; //amount of damage not taken
+    }
+
+    /**
+     * Take damage based on the enemies armor and used weapon
+     * @return a pair which contains 2 values
+     * * damage done [Float] (pair.a)
+     * * whether the enemy survived [Boolean] (pair.b)
+     */
+    public fun takeDamage(weapon: Item?): Pair<Float, Boolean>{
+        var damage = 1f;
+        if (weapon != null) { //if a weapon was used
+            if (weapon.type == ItemType.WEAPON) { //weapon is actually a weapon
+                val weaponDamage = weapon.properties["Damage"]
+                if (weaponDamage is Float){ //weapon damage is set and is a float
+                    damage = weaponDamage; //damage is weapon damage
+                }
+            }
+        }
+
+        damage *= 1-calcArmorPerc(); //multiply with armorPerc
+        //1-armor because if armor is .1 the damage should be .9 and 1- 0.1 = 0.9
+
+        health -= damage; //do damage
+
+        //return damage and
+        //whether enemy is alive (health > 0)
+        return Pair(damage, health > 0);
     }
 }

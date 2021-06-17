6d59b9c6411375ba017b5e495ed148a6b38332a7
ketrab2004 <bartek@fos.nl>
2021-06-17 12:15:30 +0200

    Added boolean for battleMode

--- src/main/kotlin/classes/Player.kt
+++ src/main/kotlin/classes/Player.kt
@@ -6,8 +6,9 @@ class Player(name: String, description: String, maxHealth: Float)
     var currentLevel: Int = 0; //index of level in gameMap array
     var currentRoom: Int = 0; //index of room in gameMap array > list
 
-    var currentMenu: MenuType = MenuType.NONE
+    var currentMenu: MenuType = MenuType.NONE;
     var currentMenuIndex: Int = 0; //if in a inventory menu this index points to which item
     var currentMenuIndex1: Int = 0; //if in a container this index points to which item inside of it
 
+    var battleMode: Boolean = false;
 }

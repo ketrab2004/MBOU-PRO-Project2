41d84bdfa7364670916e0739155fcbcc042439b6
ketrab2004 <bartek@fos.nl>
2021-06-17 16:28:42 +0200

    Made doEnemyAttacks functional Also made check for health so you can die now

--- src/main/kotlin/functions/DoEnemyAttacks.kt
+++ src/main/kotlin/functions/DoEnemyAttacks.kt
@@ -2,10 +2,28 @@ package functions
 
 import classes.Player
 import classes.Room
+import classes.item.Item
+import kotlin.system.exitProcess
 
 public fun doEnemyAttacks(plr: Player, room: Room){
-    //TODO make enemies attack player or somethign
 
-    println("Enemy attacks you woooo...")
+    room.enemyList.forEach{
+        val (weapons, _) = getWeapons(it.inventory)
+        var weapon: Item? = null;
+
+        if (weapons.size > 0){
+            weapon = weapons[0] //use first weapon in inventory as weapon
+        }
+
+        val (damage, _) = plr.takeDamage(weapon);
+        println("${it.name} did ${Math.round(damage*10) /10f} to you.")
+    }
+
+    if (plr.health <= 0){ //TODO make this better ;)
+        println("You have died.")
+        println("Any last words?")
+        readLine()
+        exitProcess(0) //close game after player typed
+    }
 
 }

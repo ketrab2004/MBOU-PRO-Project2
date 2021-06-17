cc00f3e0ff3761f0aeac3568237332c5d4c5de9d
ketrab2004 <bartek@fos.nl>
2021-06-17 16:04:13 +0200

    Created function/file for doing enemy attacks

---
+++ src/main/kotlin/functions/DoEnemyAttacks.kt
@@ -0,0 +1,11 @@
+package functions
+
+import classes.Player
+import classes.Room
+
+public fun doEnemyAttacks(plr: Player, room: Room){
+    //TODO make enemies attack player or somethign
+
+    println("Enemy attacks you woooo...")
+
+}

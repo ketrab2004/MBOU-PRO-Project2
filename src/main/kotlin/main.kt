b660615692175082cb32866630b6d4e3b5e74469
ketrab2004 <bartek@fos.nl>
2021-06-24 13:14:31 +0200

    Changed command prefixes (so it makes more sense without special characters)
    <*> is highest level
    <-> is inside something (room, inv, weapons)
    <object> is interacting with object
    <object> - is interacting with content of object
    <weapon> - is interacting with weapon/picking an enemy to attack
    <object - item> is interacting with item inside of object

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,7 +11,7 @@ fun main(args: Array<String>) {
     print("Choose your name: ")
     var player: Player = Player(readLine().toString(), "The player", 10f)
 
-    // region TEMP
+    // region TEMP //TODO remove this!
     player.inventory.add(Item.createConsumable("Food", "Tasty", 3, 1f))
     player.inventory.add(Item.createWearable("Helmet", "for your head", ItemArmorSlot.HEAD, .1f))
     player.inventory.add(Item.createKey("Golden key", "Made of gold", "key1"))
@@ -29,49 +29,49 @@ fun main(args: Array<String>) {
         print("\n") //new line so everything to do with this command is seperated
 
         if (player.battleMode){
-            print("\u203C ") //print a ‼ in front of everything while you are in battle mode
+            print("! ") //print a ! in front of everything while you are in battle mode
         }
 
         when(player.currentMenu){
             MenuType.NONE ->{
-                print("* ") //print * to type command after
+                print("<*> ") //print <*> to type command after
                 doPlayerCommand(readLine().toString(), player)
             }
 
             MenuType.INVENTORY ->{ //look through your inventory to pick an item
-                print("- ") // - to type command after
+                print("<-> ") // <-> to type command after
                 doCommandInventory(readLine().toString(), player)
             }
             MenuType.IN_INVENTORY ->{ //When you have selected an item inside of your inventory
                 val item = player.inventory[player.currentMenuIndex]
-                print("${item.name} - ") //print item name - to type command after
+                print("<${item.name}> ") //print <item name> to type command after
                 doCommandItem(readLine().toString(), item, player)
             }
 
             MenuType.ROOM ->{ //look through current room to pick a poi
-                print("- ") // - to type command after
+                print("<-> ") // <-> to type command after
                 doCommandRoom(readLine().toString(), player)
             }
             MenuType.IN_ROOM ->{
                 val poi = GlobalGameMap.gameMap[player.currentLevel][player.currentRoom].poiList[player.currentMenuIndex]
-                print("${poi.name} - ") //print poi name - to type command after
+                print("<${poi.name}> ") //print <poi name> to type command after
                 doCommandPOI(readLine().toString(), poi, player);
             }
 
             MenuType.CONTAINER ->{
                 val poi = GlobalGameMap.gameMap[player.currentLevel][player.currentRoom].poiList[player.currentMenuIndex]
-                print("${poi.name}>- ") //print poi name >- to type command after
+                print("<${poi.name}> - ") //print <poi name> - to type command after
                 doCommandContainer(readLine().toString(), player);
             }
             MenuType.IN_CONTAINER ->{
                 val poi = GlobalGameMap.gameMap[player.currentLevel][player.currentRoom].poiList[player.currentMenuIndex]
                 val inventory = poi.properties["Content"] as MutableList<Item>;
-                print("${poi.name}> ${inventory[player.currentMenuIndex1].name} - ") //print poi name >- to type command after
+                print("<${poi.name} - ${inventory[player.currentMenuIndex1].name}> ") //print <poi name - item name> to type command after
                 doCommandIn_Container(readLine().toString(), player, inventory, poi);
             }
 
             MenuType.EQUIPMENT ->{
-                print("- ") // - to type command after
+                print("<-> ") // <-> to type command after
                 doCommandEquipment(readLine().toString(), player)
             }
             MenuType.IN_EQUIPMENT ->{
@@ -80,12 +80,12 @@ fun main(args: Array<String>) {
                 if (item != null){ //check because item is item?
                     name = item.name;
                 }
-                print("$name - ") //print item name - to type command after
+                print("<$name> ") //print <item name> to type command after
                 doCommandIn_Equipment(readLine().toString(), player)
             }
 
             MenuType.ATTACK ->{
-                print("- ") // - to type command after
+                print("<-> ") // <-> to type command after
                 doCommandAttack(readLine().toString(), player)
             }
             MenuType.IN_ATTACK ->{
@@ -97,7 +97,7 @@ fun main(args: Array<String>) {
                     weapon = player.inventory[player.currentMenuIndex]
                     weaponName = weapon.name
                 }
-                print("$weaponName - ") //print weapon name * to type command after
+                print("<$weaponName> - ") //print <weapon name> - to type command after
                 doCommandIn_Attack(readLine().toString(), player, room, weapon);
             }
         }
